95eb8c570099c7bd4a597ce02a1cecfdad892109
Dawid Szczurkowski <dszczurkowski@gmail.com>
2021-05-25 19:34:50 +0200

    entities

--- src/components/Entities/Entities.tsx
+++ src/components/Entities/Entities.tsx
@@ -1,5 +1,95 @@
-import { FC } from "react";
+import React, { FC, useState, ChangeEvent } from "react";
+import styled from "styled-components";
+import { IState } from "../../reducers";
+import { useSelector } from "react-redux";
+import { IUsersReducer } from "../../reducers/usersReducer";
+import { IPhotosReducer } from "../../reducers/photosReducer";
+import { FcSettings } from "react-icons/fc";
+import { ImWindows8 } from "react-icons/im";
+import { AiOutlineInsertRowLeft } from "react-icons/ai";
+import * as AllTypes from "react-icons";
+
+const MainWrapper = styled.div`
+    display: flex;
+
+    width: 100%;
+    /* ZMIEN TO */
+    height: 100vh;
+`;
+const a = 0;
+const FirstLine = styled.div`
+    margin: 15px 0 0 0;
+    height: 5%;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+    flex-direction: row;
+`;
+const Text = styled.div`
+    display: flex;
+    font-size: 24px;
+`;
+const SwapView = styled.button`
+    border: none;
+    display: flex;
+    background-color: #9ee4ff8f;
+    height: 30px;
+    justify-content: center;
+    align-items: center;
+    width: 94px;
+    color: royalblue;
+`;
+const Margin = styled.span`
+    margin-right: 10px;
+`;
 
 export const Entities: FC = () => {
-    return <>Entities</>;
+    const { photosList } = useSelector<IState, IPhotosReducer>(
+        (globalState) => ({
+            ...globalState.photos,
+        })
+    );
+    const [mosaic, setMosaic] = useState<boolean>(true);
+    const data: any = [];
+    for (let i: any = 0; i < 30; i++) {
+        data.push(photosList?.[i]);
+    }
+    console.log(data);
+    return (
+        <MainWrapper>
+            <FirstLine>
+                <Text>
+                    <Margin>Entities</Margin> <FcSettings />
+                </Text>
+                {mosaic ? (
+                    <SwapView onClick={() => setMosaic(false)}>
+                        <Margin>Mosaic </Margin>
+                        <ImWindows8 />
+                    </SwapView>
+                ) : (
+                    <SwapView onClick={() => setMosaic(true)}>
+                        <Margin>Row </Margin>
+                        <AiOutlineInsertRowLeft />
+                    </SwapView>
+                )}
+            </FirstLine>
+            <div>
+                {data.map((photo: any) => (
+                    <div>
+                        <p>{photo.id}</p>
+                        <img src={photo.url} />
+                    </div>
+                ))}
+            </div>
+            {/* <div>
+                {data.map((photo: any, x: any) => (
+                    <div key={x}>
+                        <p>{photo.id}</p>
+                        <p>{photo.url}</p>
+                    </div>
+                ))}
+            </div> */}
+        </MainWrapper>
+    );
 };
